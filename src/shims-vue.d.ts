5a8ce2c4c90440270cfe67920bd2877b68bf9b43
phai-le <phai.le@tomosia.com>
2025-06-13 15:58:25 +0700

    Fix

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,17 @@ declare module "*.vue" {
     const component: DefineComponent<{}, {}, any>
     export default component
 }
+
+import { VNode } from 'vue';
+
+declare global {
+    namespace JSX {
+        interface Element extends VNode { }
+        interface ElementClass {
+            $props: any;
+        }
+        interface IntrinsicElements {
+            [elem: string]: any;
+        }
+    }
+}
